6c533e14cf8c93a74a8c8753306390f8f04d0209
Vinícius Souza <vinicius.sfs91@gmail.com>
2021-06-01 11:52:39 -0300

    primeiro commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+export function Header() {
+    return(
+        <>
+        <h1 key>Localize uma Org - Github</h1>
+		<div class="content-form">
+			<form>
+				<input type="" id="input_user" placeholder="Usuário do Github" />
+				<button type="submit" class="btn-app">Buscar</button>
+			</form>
+		</div>
+        </>
+    )
+}
